2454fab6299648fee49ae88d84b4493a5d46bad0
zhengshugan <zhengshugan@foxmail.com>
2022-09-25 20:14:17 +0800

    add axios response interceptors

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,8 @@
 // 基于 axios 封装的网络请求函数
 import axios from "axios";
 import store from '@/store'
+import router from '@/router'
+import { Message } from 'element-ui'
 
 export const baseURL = 'http://big-event-vue-api-t.itheima.net'
 const myAxios = axios.create({
@@ -12,6 +14,7 @@ const myAxios = axios.create({
  * 定义请求拦截器
  *
  * @param config 配置对象（要请求的后台参数都在这个参数上）
+ * @param error 错误对象
  * */
 myAxios.interceptors.request.use((config) => {
   // 在请求前会触发一次
@@ -26,6 +29,36 @@ myAxios.interceptors.request.use((config) => {
   return config
 }, (error) => {
   // 请求前发起的代码，如果有异常会直接进入这里
+  return Promise.reject(error)
+})
+
+/**
+ * 定义响应拦截器
+ *
+ * @param response 响应成功的结果
+ * @param error 响应失败的对象
+ * */
+myAxios.interceptors.response.use((response) => {
+  // http 响应状态码为 2xx 或 3xx 触发处理成功的回调
+  // response 是响应成功的结果
+  return response
+}, (error) => {
+  // http 响应状态码为 4xx 或 5xx 触发处理失败的回调
+  // error 是响应错误的结果
+
+  // token 过期
+  if (error.response.status === 401) {
+    // 清除 Vuex 里的 store
+    store.commit('updateToken', '')
+    store.commit('updateUserInfo', { })
+
+    // 切换到登陆页
+    router.push('/login')
+
+    // 弹窗提示
+    Message.error('用户过期，请重新登陆！')
+  }
+
   return Promise.reject(error)
 })
 
